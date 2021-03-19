1e93b4855c6b84217787573d5a71f125a293f3ba
cRohrer82 <80014660+cRohrer82@users.noreply.github.com>
2021-03-19 07:02:53 -0400

    Add files via upload

---
+++ Calculator_CSonly/MathOP.cs
@@ -0,0 +1,59 @@
+﻿/*********************************
+ Name:          Christopher Rohrer
+ Program name:  Calculator
+ Date:          2 June 2019
+ Purpose:       Week4 Assignment
+*********************************/
+using System;
+
+class MathOP
+{
+    protected double First;
+    protected double Second;
+    protected char Operation;
+
+    public MathOP()
+    {
+        First = 0;
+        Second = 0;
+        Operation = Convert.ToChar("+");
+    }
+
+    public MathOP(double first, double second, char operation)
+    {
+        First = first;
+        Second = second;
+        Operation = operation;
+    }
+
+    public virtual double GetResult()
+    {
+        // Run 'MathAdd' if operation is plus sign, else run 'MathSub'
+        if (Operation == Convert.ToChar("+"))
+        {
+            return MathAdd();
+        }
+        else
+        {
+            return MathSub();
+        }
+    }
+
+    private double MathAdd()
+    {
+        double result;
+
+        // Perform addition on user data and return result
+        result = First + Second;
+        return result;
+    }
+
+    private double MathSub()
+    {
+        double result;
+
+        // Perform subtraction on user data and return result
+        result = First - Second;
+        return result;
+    }
+}
---
+++ Calculator_CSonly/MathOP2.cs
@@ -0,0 +1,55 @@
+﻿/*********************************
+ Name:          Christopher Rohrer
+ Program name:  Calculator
+ Date:          2 June 2019
+ Purpose:       Week4 Assignment
+*********************************/
+using System;
+
+class MathOP2: MathOP
+{
+    public MathOP2()
+    {
+        First = 0;
+        Second = 0;
+        Operation = Convert.ToChar("*");
+    }
+
+    public MathOP2(double first, double second, char operation)
+    {
+        First = first;
+        Second = second;
+        Operation = operation;
+    }
+
+    public override double GetResult()
+    {
+        // Run 'MathMult' if operation is plus sign, else run 'MathDiv'
+        if (Operation == Convert.ToChar("*"))
+        {
+            return MathMult();
+        }
+        else
+        {
+            return MathDiv();
+        }
+    }
+    
+    private double MathMult()
+    {
+        double result;
+
+        // Perform multiplication on user data and return result
+        result = First * Second;
+        return result;
+    }
+
+    private double MathDiv()
+    {
+        double result;
+
+        // Perform division on user data and return result
+        result = First / Second;
+        return result;
+    }
+}
